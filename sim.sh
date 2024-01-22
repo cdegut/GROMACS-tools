769c9cfc779f395fa51d13fc2b02fed83d14d1b1
Clément Dégut <58604417+cdegut@users.noreply.github.com>
2024-01-22 14:04:27 +0000

    Update sim.sh

--- sim.sh
+++ sim.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-gmx grompp -f /local_scratch/clement/gromacs/GROMACS-tools/mdpfiles/md250ns.mdp -c md1ns.gro -t md1ns.cpt -p topol.top -o md250ns.tpr
+mdp_files_folder=/local_scratch/clement/gromacs/GROMACS-tools/mdpfiles
+gmx grompp -f $mdp_files_folde/md250ns.mdp -c md1ns.gro -t md1ns.cpt -p topol.top -o md250ns.tpr
 gmx mdrun -pme gpu -v -deffnm md250ns
 echo "1 1" | gmx trjconv -s md250ns.tpr -f md250ns.xtc -o md250ns_center_po.xtc -center -pbc mol -ur compact
 gmx trjconv -s md250ns.tpr -f md250ns_center_po.xtc -o md250ns_po_start.pdb -dump 0
