e86118cc037c53aba601d6981d49f552290febd0
Clément Dégut <58604417+cdegut@users.noreply.github.com>
2024-04-05 17:59:25 +0100

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+
 mdp_files_folder=/local_scratch/clement/gromacs/GROMACS-tools/mdpfiles
+
+
+if [ -z "$1" ]; then
+    echo "Usage: $0 input.pdb"
+    exit 1
+fi
+
 gmx pdb2gmx -ignh -f $1 -o starting_structure.gro
 gmx editconf -f starting_structure.gro -o struct_new_box -c -d 1.0 -bt dodecahedron
 gmx solvate -cp struct_new_box.gro -cs tip4p -o struct_box_water.gro -p topol.top
 gmx grompp -f $mdp_files_folder/ions.mdp -c struct_box_water.gro -p topol.top -o ions.tpr
-gmx genion -s ions.tpr -o struct_box_water_ions.gro -p topol.top -pname NA -nname CL -neutral
+
+echo "Salt Concnetration in M:"
+read userInput
+gmx genion -s ions.tpr -o struct_box_water_ions.gro -p topol.top -pname NA -nname CL -neutral -conc $userInput
+
 ##Run minimization
 gmx grompp -f  $mdp_files_folder/minim.mdp -c struct_box_water_ions.gro -p topol.top -o em.tpr
 gmx mdrun -v -deffnm em
@@ -25,3 +37,7 @@ mv nvt* run_prep/
 mv npt* run_prep/
 mv ions.tpr run_prep/
 mv struct_* run_prep/
+mv starting_structure.gro run_prep/.
+mv posre.itp run_prep/.
+mv *.log run_prep/.
+mv *#* run_prep/.
